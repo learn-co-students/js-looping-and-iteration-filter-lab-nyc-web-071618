66765505713941c8c48df6e5de9c402809af4888
trisjones <trisjones04@gmail.com>
2018-08-26 21:21:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+function findMatching(drivers, name) {
+  return drivers.filter(function(driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, string) {
+  let partialName = string.length;
+  return drivers.filter(function(driverName) {
+    return driverName.slice(0, partialName) === string;
+  });
+}
+
+function matchName (list, name) {
+  return list.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
