cc20d9702950baad247eca8fe08eefbea0689e58
cmcorrales <cmcorrales3@gmail.com>
2018-08-27 15:26:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file
+// const result = words.filter(word => word.length > 6);
+function findMatching(drivers, name) {
+  const result = drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  return result;
+}
+
+function fuzzyMatch(drivers, name) {
+  const result = drivers.filter(driver => driver.slice(0, 1) === name.slice(0, 1));
+  return result;
+}
+
+function matchName(drivers, name) {
+  const result = drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+  return result;
+}
